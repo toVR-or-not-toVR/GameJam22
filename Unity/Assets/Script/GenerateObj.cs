480c7fc544537ac88b2d6a2d756b1e72e0d38451
Shon Roman <kserox228@gmail.com>
2022-01-22 15:12:23 +0800

    [ADD] RandomGenerator

--- Unity/Assets/Script/GenerateObj.cs
+++ Unity/Assets/Script/GenerateObj.cs
@@ -6,17 +6,42 @@ public class GenerateObj : MonoBehaviour
 {
 
 
-    [SerializeField] GameObject ObjGenerate; 
+    [SerializeField] List<GameObject> objectToGenerate;
+    [SerializeField] float timeToGenerateObjects = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(GenerateObjects(timeToGenerateObjects));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+    IEnumerator GenerateObjects(float timeToGerenate)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeToGerenate);
+
+            //get amount of objects Co2 on scene
+
+            //no Need
+            /*            var objSeeker = GameObject.FindGameObjectsWithTag("CO2");
+                        numberOfObjects = objSeeker.Length;*/
+
+            /*            if (numberOfObjects < maxAmountOfObjectsOnScene) //Chenck amount on scene 
+                        {*/
+
+
+
+            Instantiate(objectToGenerate[Random.Range(0, objectToGenerate.Count)], transform.position, Quaternion.identity);
+            //}
+        }
     }
 }
